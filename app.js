a815385f5333e0c7fb478163ceea4999e434ee35
Danish Iqbal <danishiqbalscorpio@gmail.com>
2016-01-23 02:33:30 +0500

    Added api file

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var client_location,
     client_latitude,
     client_longtitude,
-    myLatLng;
+    myLatLng,
+    beaches;
 
 $.get("http://ipinfo.io", function(response) {
     client_location = response.loc;
     client_latitude = client_location.slice(0, 7);
     client_longtitude = client_location.substr(client_location.length - 7);
-    if(client_longtitude > 0) {
-      initMap();
-    }
+    // if(client_longtitude > 0) {
+    //   initMap();
+    // }
 }, "jsonp");
 
-
+$.get("https://api.myjson.com/bins/1fffj" , function(response) {
+    console.log(response.beaches[0]);
+    beaches = response.beaches;
+    if(beaches.length > 0) {
+      initMap();
+    }
+})
 
 
 
@@ -43,7 +50,7 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
-    center: myLatLng,
+    center: {lat: -33.890542, lng: 151.274856},
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
     }
@@ -52,10 +59,19 @@ function initMap() {
 
 
   var marker = new google.maps.Marker({
-    position: myLatLng,
+    position: {lat: -33.890542, lng: 151.274856},
     map: map,
     title: 'Hello World!'
   });
+
+  for (var i = 0; i < beaches.length; i++) {
+    var beach = beaches[i];
+    var marker = new google.maps.Marker({
+      position: {lat: beach[1], lng: beach[2]},
+      map: map,
+      title: beach[0]
+    });
+  }
 
   map.mapTypes.set(customMapTypeId, customMapType);
   map.setMapTypeId(customMapTypeId);
